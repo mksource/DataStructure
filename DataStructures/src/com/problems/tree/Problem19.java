833935afce2ecafafab0a48266e8f3ad6cbc3df0
mknarayan1711 <mknarayan1711@gmail.com>
2014-05-07 16:47:40 +0530

    Tree problem

---
+++ DataStructures/src/com/problems/tree/Problem19.java
@@ -0,0 +1,56 @@
+package com.problems.tree;
+
+public class Problem19 {
+
+	
+	private int findLevel(int levels[],int arr[],int elem){
+		
+		//If the root element is found
+		if(arr[elem]==-1){
+			 levels[elem]=0;
+			 return 1;
+		}	 
+		
+		//If the parent element of this element is found
+		if(levels[elem]!=-1){
+			return levels[elem]+1;
+		}
+		
+		
+		return findLevel(levels,arr,arr[elem]);
+			
+	}
+	
+	private void printReverseLevelOrder(int arr[]){
+		
+		int n=arr.length;
+		int levels[]=new int[n];
+		
+		
+		for(int i=0;i<n;i++){
+			levels[i]=-1;
+		}
+		
+		for(int i=0;i<n;i++){
+			
+			int parent=arr[i];
+			
+		    //If Parent is equal to -1
+			if(parent==-1)
+				levels[i]=0;
+				
+			levels[i]=findLevel(levels,arr,parent);
+			
+			
+		}
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
